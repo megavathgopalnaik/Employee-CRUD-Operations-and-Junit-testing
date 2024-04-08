2cec6b3b60ae75f47f30cf85fa7a2ef55e7b19be
Gopal Naik <gopalnaikmegavath1@gmail.com>
2024-04-08 16:39:20 +0530

    updated

--- src/main/java/employee/exceptionadvice/LoggingAspect.java
+++ src/main/java/employee/exceptionadvice/LoggingAspect.java
@@ -2,14 +2,20 @@ package employee.exceptionadvice;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
 
 import employee.exception.EmployeeException;
 
+@Component
+@Aspect
 public class LoggingAspect {
 	
 	
 	    private static final Log LOGGER = LogFactory.getLog(LoggingAspect.class);
 
+	    @AfterThrowing(pointcut = "execution(* employee.service.EmployeeServiceImpl.*(..))", throwing = "exception")
 	    public void logServiceException(EmployeeException exception) {
 	        LOGGER.error(exception.getMessage(), exception);
 	  
